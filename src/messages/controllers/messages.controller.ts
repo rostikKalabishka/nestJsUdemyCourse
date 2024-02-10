7cc69dcef389a6869a852232bf360f21197e0c61
RostikKalabishka <rostik31052002@gmail.com>
2024-02-10 19:18:41 +0200

    first commit

---
+++ src/messages/controllers/messages.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessage() {}
+
+  @Post()
+  createMessage() {}
+
+  @Get('/:id')
+  getMessage() {}
+}
